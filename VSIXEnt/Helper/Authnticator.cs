522b4ebb325568b9e40b58d3877caf9c14be6fb2
TheArslan <arslanafzalshani@gmail.com>
2019-09-01 00:59:03 +0500

    My very first Commit

---
+++ VSIXEnt/Helper/Authnticator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIXEnt.Helper
+{
+    class Authnticator
+    {
+        string userStatus;
+        public static bool isUserLogedIn()
+        {
+            return true;
+        }
+    }
+}
